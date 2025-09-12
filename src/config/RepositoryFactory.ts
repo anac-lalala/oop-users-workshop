78caa5d5c2f15502fd9697dfbb541b5e98395c43
Ana Caicedo <ana.caicedo@edgeuno.com>
2025-09-12 18:57:19 -0500

    crear singleton para consistencia en memoria - SOLO DEV

--- src/config/RepositoryFactory.ts
+++ src/config/RepositoryFactory.ts
@@ -1,11 +1,20 @@
 import { IUserRepository } from "@/domain/repositories/IUserRepository";
 import { env } from "./env";
 import { InMemoryUserRepository } from "@/infrastructure/repositories/InMemoryUserRepository";
+import { FileRepository } from "@/infrastructure/repositories/FileRepository";
+
+let singletonRepo: IUserRepository | null = null;
 
 export function createUserRepository(): IUserRepository {
+  if (singletonRepo) return singletonRepo;
+
   switch (env.REPO_TYPE) {
+    case "file":
+      singletonRepo = new FileRepository(env.FILE_PATH);
+      break;
     case "memory":
     default:
-      return new InMemoryUserRepository();
+      singletonRepo = new InMemoryUserRepository();
   }
+  return singletonRepo;
 }
