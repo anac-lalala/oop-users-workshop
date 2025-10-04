d6e32815f215d640607835a53091ad1474c68ed7
Ana Caicedo <ana.caicedo@edgeuno.com>
2025-10-04 00:33:25 -0500

    Replace RepositoryFactory with SaveUserCreator for user repository instantiation

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,14 +1,12 @@
 import { RegisterUserUseCase } from "@/application/use-cases/RegisterUserUseCase";
 import { env } from "@/config/env";
-import { RepositoryFactory } from "@/config/RepositoryFactory";
+import { SaveUserCreator } from "@/domain/factories/SaveUserCreator";
 import { RegisterRouteHandler } from "@/domain/handlers/RegisterRouteHandler";
 import { BcryptPasswordHasher } from "@/infrastructure/services/BcryptPasswordHasher";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const createUserRepository = RepositoryFactory.createUserRepository(
-    env.REPO_TYPE
-  );
+  const createUserRepository = SaveUserCreator.createSaveUser(env.REPO_TYPE);
   const bcryptPasswordHasher = new BcryptPasswordHasher();
 
   const registerUserUseCase = new RegisterUserUseCase(
